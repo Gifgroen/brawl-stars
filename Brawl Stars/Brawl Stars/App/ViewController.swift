f7ddad2f2a30624c923e6dd730d8ea4ad46f9d13
Karsten Westra <karstenwestra@gmail.com>
2022-10-26 22:48:50 +0200

    Added blank Xcode project with UIKit (without Storyboard).

---
+++ Brawl Stars/Brawl Stars/App/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Brawl Stars
+//
+//  Created by Karsten Westra on 26/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+}
